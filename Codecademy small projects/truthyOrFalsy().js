c962f5538398794ee89c9ae78df8f0e8e33d169c
Amin <amin_ghs@yahoo.com>
2020-05-08 16:47:31 +0200

    Create truthyOrFalsy().js

---
+++ Codecademy small projects/truthyOrFalsy().js
@@ -0,0 +1,25 @@
+// Write your function here:
+
+const truthyOrFalsy = (a) => {
+    if (a) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  
+  
+  
+  
+  // Uncomment the line below when you're ready to try out your function
+  console.log(truthyOrFalsy(0)) // Should print false
+  
+  // We encourage you to add more function calls of your own to test your code!
+  
+  
+  
+  
+  
+  
+  
