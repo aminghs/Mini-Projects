9d897c8cecac32e13e8dee1c881a9f11aed9e1ef
Amin <amin_ghs@yahoo.com>
2020-05-08 16:50:46 +0200

    Create sillySentence().js

---
+++ Codecademy small projects/sillySentence().js
@@ -0,0 +1,15 @@
+// Write your function here:
+
+const sillySentence = (a, b, c) => {
+    return `I am so ${a} because I ${b} coding! Time to write some more awesome ${c}!`;
+  }
+  
+  
+  
+  
+  
+  // Uncomment the line below when you're ready to try out your function
+  console.log(sillySentence('excited', 'love', 'functions')) 
+  
+  // We encourage you to add more function calls of your own to test your code!
+  
