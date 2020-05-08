2b73fe9f6aae901e54d1625254e064b475cef83f
Amin <amin_ghs@yahoo.com>
2020-05-08 16:47:24 +0200

    Create numImaginaryFriends().js

---
+++ Codecademy small projects/numImaginaryFriends().js
@@ -0,0 +1,14 @@
+// Write your function here:
+
+const numImaginaryFriends = (numberOfFriends) =>{
+    return Math.round((numberOfFriends * 33) / 100);
+};
+
+
+
+
+
+// Uncomment the line below when you're ready to try out your function
+console.log(numImaginaryFriends(18)) // Should print 6
+
+// We encourage you to add more function calls of your own to test your code!
