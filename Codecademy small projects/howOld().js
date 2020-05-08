b65b718950dac0ba428d9ee2d358df26f421bec7
Amin <amin_ghs@yahoo.com>
2020-05-08 17:18:58 +0200

    Create howOld().js

---
+++ Codecademy small projects/howOld().js
@@ -0,0 +1,20 @@
+/* 
+Our solution is written as a function expression and uses string interpolation, but it would be equally acceptable to use a function declaration and/or string concatenation
+*/
+
+const howOld = (age, year) => {
+    // The following two lines make it so that our function always knows the current year.
+        let dateToday = new Date();
+        let thisYear = dateToday.getFullYear();
+    // It is totally ok if your function used the current year directly!
+      
+        const yearDifference = year - thisYear
+        const newAge = age + yearDifference
+        if (newAge < 0) {
+            return `The year ${year} was ${-newAge} years before you were born`
+        } else if (newAge > age) {
+            return `You will be ${newAge} in the year ${year}`
+        } else {
+            return `You were ${newAge} in the year ${year}`
+        }
+    }
