eca4b301888cffd9f8568401b352e4ed8d50a417
Amin <amin_ghs@yahoo.com>
2020-05-17 16:02:25 +0200

    Create reverseArray().js

---
+++ Codecademy small projects/INTERMEDIATE JAVASCRIPT/reverseArray().js
@@ -0,0 +1,32 @@
+const reverseArray = arr => {
+    let reversed = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    return reversed
+}
+/*
+// Alternate solutions:
+
+// Using the .unshift() method
+const reverseArray = arr => {
+    let reversed = [];
+    for (let i = 0; i < arr.length; i++) {
+        reversed.unshift(arr[i]);
+    }
+    return reversed
+}
+
+// As a function declaration:
+function reverseArray(arr) {
+    let reversed = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    return reversed
+}
+*/
+
+const sentence = ['sense.','make', 'all', 'will', 'This'];
+
+console.log(reverseArray(sentence)); 
