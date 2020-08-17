790aa3e52bb05f49352eba0d79cb04abba98de99
Amin <amin_ghs@yahoo.com>
2020-08-17 15:31:15 +0200

    Update server.js

--- Zoom-video-chat/server.js
+++ Zoom-video-chat/server.js
@@ -16,3 +16,9 @@ app.get('/:room', (req, res) => {
 })
 
 server.listen(3000)
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        console.log(roomId, userId);        
+    })
+})
