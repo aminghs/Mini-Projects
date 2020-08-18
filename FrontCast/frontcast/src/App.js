ac1416d26a06ce99618634ac51cbbe81c2ec47fd
Amin <amin_ghs@yahoo.com>
2020-08-18 13:29:56 +0200

    Using useState as a Hook

--- FrontCast/frontcast/src/App.js
+++ FrontCast/frontcast/src/App.js
@@ -1,46 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from './components/Product/Product'
 
-class App extends React.Component {
 
-    state = {
+const App = props => {
+
+    const [productState, setProductState] = useState({
         products: [
-            { title: 'Book 1', price: 99},
-            { title: 'Book 2', price: 89},
-            { title: 'Book 3', price: 79},
-        ],
-    }
+                    { title: 'Book 1', price: 99},
+                    { title: 'Book 2', price: 89},
+                    { title: 'Book 3', price: 79},
+                ],
+    })
 
-    changePriceHandler = () => {
-        this.setState({
+    const changePriceHandler = () => {
+        setProductState({
             products: [
-                { title: 'Book 1', price: 59},
-                { title: 'Book 2', price: 69},
-                { title: 'Book 3', price: 29},
-            ],  
+                                { title: 'Book 1', price: 59},
+                                { title: 'Book 2', price: 69},
+                                { title: 'Book 3', price: 29},
+                            ],
         })
     }
 
-    render() {
-        return (
-            <div id="main" className="container">
-                <h2>React App</h2>
-                <Product
-                    title={this.state.products[0].title} 
-                    price={this.state.products[0].price} 
-                />
-                <Product 
-                    title={this.state.products[1].title} 
-                    price={this.state.products[1].price} 
-                />
-                <Product 
-                    title={this.state.products[2].title} 
-                    price={this.state.products[2].price} 
-                />
-                <button onClick={this.changePriceHandler} >Change price</button>
-            </div>
-        )
-    }
+    return (
+                    <div id="main" className="container">
+                        <h2>React App</h2>
+                        <Product
+                            title={productState.products[0].title} 
+                            price={productState.products[0].price} 
+                        />
+                        <Product 
+                            title={productState.products[1].title} 
+                            price={productState.products[1].price} 
+                        />
+                        <Product 
+                            title={productState.products[2].title} 
+                            price={productState.products[2].price} 
+                        />
+                        <button onClick={changePriceHandler} >Change price</button>
+                    </div>
+                )
 }
+
+
+// class App extends React.Component {
+
+//     state = {
+//         
+//     }
+
+//     changePriceHandler = () => {
+//         this.setState({
+//             products: [
+//                 { title: 'Book 1', price: 59},
+//                 { title: 'Book 2', price: 69},
+//                 { title: 'Book 3', price: 29},
+//             ],  
+//         })
+//     }
+
+//     render() {
+//         
+//     }
+// }
 
   export default App;
