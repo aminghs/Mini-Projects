237b963d28f752d7788086ff8d6d60520264561c
Amin Ghs <amin_ghs@yahoo.com>
2021-12-21 15:14:54 +0100

    removeTour function

--- Tours/src/App.js
+++ Tours/src/App.js
@@ -8,6 +8,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour)=> tour.id !== id);
+    setTours(newTours);
+  }
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -34,9 +39,19 @@ function App() {
       </main>
     )
   }
+  if (tours.length === 0) {
+    return <main>
+      <div className='title'>
+        <h2>no tour left</h2>
+        <button className='btn' onClick={fetchTours}>
+          Refresh 
+        </button>
+      </div>
+    </main>
+  }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   )
 }
